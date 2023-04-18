7200517edb5e81a7b6a0c7520a103a48b3ec83e0
farelibrahim34 <105421371+farelibrahim34@users.noreply.github.com>
2023-04-18 21:06:45 +0700

    update add data+halaman detail

--- app/src/main/java/com/projectfarrel/infokosadmin/adapter/AdapterDataKos.kt
+++ app/src/main/java/com/projectfarrel/infokosadmin/adapter/AdapterDataKos.kt
@@ -1,11 +1,13 @@
 package com.projectfarrel.infokosadmin.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.projectfarrel.infokosadmin.databinding.ItemBinding
 import com.projectfarrel.infokosadmin.model.ResponseDataKosItem
+import com.projectfarrel.infokosadmin.view.isi.DetailActivity
 
 class AdapterDataKos(private var listData:List<ResponseDataKosItem>): RecyclerView.Adapter<AdapterDataKos.ViewHolder>() {
     class ViewHolder(var binding : ItemBinding):RecyclerView.ViewHolder(binding.root) {
@@ -22,6 +24,20 @@ class AdapterDataKos(private var listData:List<ResponseDataKosItem>): RecyclerVi
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.txtNamaKos.text = listData[position].namaKos
         Glide.with(holder.itemView).load(listData[position].fotoKos).fitCenter().into(holder.binding.ivImage)
+
+        holder.binding.cardList.setOnClickListener {
+            val detail = Intent(it.context, DetailActivity::class.java)
+            detail.putExtra("id",listData[position].id)
+            detail.putExtra("fotokos",listData[position].fotoKos)
+            detail.putExtra("nama",listData[position].namaKos)
+            detail.putExtra("alamat",listData[position].alamat)
+            detail.putExtra("foto1",listData[position].fotoSatu)
+            detail.putExtra("foto2",listData[position].fotoDua)
+            detail.putExtra("foto3",listData[position].fotoTiga)
+            detail.putExtra("nohp",listData[position].noHp)
+            it.context.startActivity(detail)
+        }
+
     }
     override fun getItemCount(): Int {
         return listData.size
--- app/src/main/java/com/projectfarrel/infokosadmin/view/isi/DetailActivity.kt
+++ app/src/main/java/com/projectfarrel/infokosadmin/view/isi/DetailActivity.kt
@@ -3,10 +3,35 @@ package com.projectfarrel.infokosadmin.view.isi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.projectfarrel.infokosadmin.R
+import com.projectfarrel.infokosadmin.databinding.ActivityDetailBinding
+import com.projectfarrel.infokosadmin.viewmodel.ViewModelDataKos
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailActivity : AppCompatActivity() {
+    lateinit var binding : ActivityDetailBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setDetail()
+
+    }
+    private fun setDetail(){
+        val nama = intent.getStringExtra("nama")
+        val fotokos = intent.getStringExtra("fotokos")
+        val alamat = intent.getStringExtra("alamat")
+        val foto1 = intent.getStringExtra("foto1")
+        val foto2 = intent.getStringExtra("foto2")
+        val foto3 = intent.getStringExtra("foto3")
+        val nohp = intent.getStringExtra("nohp")
+
+        binding.txtKos.setText(nama)
+        binding.txtAlamat.setText(alamat)
+        binding.btnNomorHp.setText(nohp)
+        binding.viewPagerHomeDetail
     }
 }
